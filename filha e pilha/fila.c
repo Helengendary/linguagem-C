3af1caec966e91d070392d76401fb3eeef385107
HelenaPicinin <137627315+HelenaPicinin@users.noreply.github.com>
2024-05-16 16:54:12 -0300

    fila em c

---
+++ filha e pilha/fila.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "fila.h"
+
+int main() {
+
+    Enqueue(5);
+    Enqueue(8);
+    Enqueue(2);
+    Enqueue(0);
+    Enqueue(9);
+    Enqueue(1);
+    GetSize();
+    get(0);
+    Dequeue();
+    get(0);
+    ultimo();
+}
