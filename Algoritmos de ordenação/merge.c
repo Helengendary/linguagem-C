485efa600f046085eea20a2d6576e1c65fae91fe
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-06-13 15:52:43 -0300

    quick

--- Algoritmos de ordenação/merge.c
+++ Algoritmos de ordenação/merge.c
@@ -1,18 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void Merge(int array[], int tam) {
+void merge(int arr[], int l, int m, int r) {
+  int i, j, k;
+  int n1 = m - l + 1;
+  int n2 = r - m;
 
-    if (tam == 1) {
-        printf("%d ", array[0]);
-        return;
+  int L[n1], R[n2];
+
+  for (i = 0; i < n1; i++)
+    L[i] = arr[l + i];
+  for (j = 0; j < n2; j++)
+    R[j] = arr[m + 1 + j];
+
+  i = 0;
+  j = 0;
+  k = l;
+  while (i < n1 && j < n2) {
+    if (L[i] <= R[j]) {
+      arr[k] = L[i];
+      i++;
+    } else {
+      arr[k] = R[j];
+      j++;
     }
+    k++;
+  }
 
-    int sizePrimeira = tam/2;
-    int sizeSegunda = tam/2 + (tam%2); 
+  while (i < n1) {
+    arr[k] = L[i];
+    i++;
+    k++;
+  }
 
-    Merge(array, sizePrimeira);
-    Merge(array+sizePrimeira, sizeSegunda);
+  while (j < n2) {
+    arr[k] = R[j];
+    j++;
+    k++;
+  }
+}
+
+void mergeSort(int arr[], int l, int r) {
+  if (l < r) {
+    int m = l + (r - l) / 2;
+
+    mergeSort(arr, l, m);
+    mergeSort(arr, m + 1, r);
+
+    merge(arr, l, m, r);
+  }
 }
 
 int main() {
